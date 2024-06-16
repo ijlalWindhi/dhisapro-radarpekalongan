b3b2ad86bf6bf5a104c5c6fd60573ca664e77d0b
BayuAditiyaa <shinatsuka7@gmail.com>
2024-06-16 15:31:22 +0700

    third commit

--- app/(admin)/layout.jsx
+++ app/(admin)/layout.jsx
@@ -1,5 +1,4 @@
 import "../globals.css";
-import Coba from "./dashboard/coba";
 import HeaderDashboard from "./dashboard/components/HeaderDashboard";
 
 export default function AdminLayout({ children }) {
