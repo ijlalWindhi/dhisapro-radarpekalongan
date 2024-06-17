4bbe5089c15ca90027ec97f60f30c68809f5d33d
BayuAditiyaa <shinatsuka7@gmail.com>
2024-06-17 21:12:05 +0700

    cls footer 2

--- app/(home)/Footer.jsx
+++ app/(home)/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:flex-col sm:items-center sm:justify-between">
+        <div className="flex justify-between sm:flex-col sm:items-center sm:justify-between">
           <Link
             href={"/"}
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
@@ -18,7 +18,7 @@ export default function Footer() {
               RADAR PEKALONGAN
             </span>
           </Link>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 sm:my-4">
             <li>
               <Link
                 href={"/Tentang-kami"}
